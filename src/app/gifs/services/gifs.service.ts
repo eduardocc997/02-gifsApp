ee762ebd7826815661e89ef6230bc95ccf207f3d
eduardocc97 <73180505+eduardocc97@users.noreply.github.com>
2021-09-13 11:08:13 -0500

    Se muestran los resultados - Se llego hasta el video 87 LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, GifsSearchResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey: string = "NmxRsCCB1qAeBM3wxT7mmLsQp8TPJHdd";   //API de GIPHY
   private _historial: string[] = [];
+
+  
+  public resultados: Gif[] = [];
 
   get historial(){
     return [...this._historial];
+  }
+
+  constructor(private http: HttpClient){
+    if(localStorage.getItem('historial')){
+      this._historial = JSON.parse(localStorage.getItem('historial')!) || []; //El signo ! le dice que omita el error, ya que nosotros estamos haciendo la validacion por lo que sabemos que no puede crashear.
+    }
   }
 
   buscargGifs(query: string){
@@ -18,7 +30,14 @@ export class GifsService {
     if(!this._historial.includes(query)){
     this._historial.unshift(query);
     this._historial = this.historial.slice(0,10); //Para que solo deje meter 10 valores
+
+    localStorage.setItem('historial', JSON.stringify(this._historial));
+
     }
-    console.log(this._historial);
+    
+    this.http.get<GifsSearchResponse>(`https://api.giphy.com/v1/gifs/search?api_key=NmxRsCCB1qAeBM3wxT7mmLsQp8TPJHdd&q=${query}&limit=10`)
+      .subscribe( (resp) =>{
+        this.resultados = resp.data;
+      });
   }
 }
